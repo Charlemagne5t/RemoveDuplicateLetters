100834c9dce3c21a6ee788ee4f292a8daa6fca77
alek <wait82985@gmail.com>
2023-09-26 16:38:50 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class Solution {
     public String removeDuplicateLetters(String s) {
-        return null;
+        Stack<Character> stack = new Stack<>();
+        char[] array = s.toCharArray();
+        int[] count = new int[26];
+        for (char ch : array) {
+            count[ch - 'a']++;
+        }
+        boolean[] visited = new boolean[26];
+
+        for (char ch : array) {
+            count[ch - 'a']--;
+            if(visited[ch- 'a']){
+                continue;
+            }
+
+            while (!stack.isEmpty() && stack.peek() > ch && count[stack.peek() -'a'] > 0){
+                visited[stack.peek() - 'a'] = false;
+                stack.pop();
+            }
+            stack.push(ch);
+            visited[ch - 'a'] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()){
+            sb.insert(0, stack.pop());
+        }
+
+        return sb.toString();
     }
 }
